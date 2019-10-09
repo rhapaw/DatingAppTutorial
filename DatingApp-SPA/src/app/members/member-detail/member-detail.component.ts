0dbcd8116e62c004d31be5435b923d15019730b3
Kevin Mitchell <kevin475@hotmail.com>
2019-10-08 19:49:48 -0700

    After Section 16

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -9,6 +9,7 @@ import {
   NgxGalleryAnimation
 } from 'ngx-gallery';
 import { TabsetComponent } from 'ngx-bootstrap';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -24,7 +25,8 @@ export class MemberDetailComponent implements OnInit {
   constructor(
     private userService: UserService,
     private alertify: AlertifyService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
@@ -68,4 +70,16 @@ export class MemberDetailComponent implements OnInit {
   selectTab(tabId: number) {
     this.memberTabs.tabs[tabId].active = true;
   }
+
+  sendLike(id: number)
+  {
+    this.userService.sendLike(this.authService.decodedToken.nameid , id).subscribe(data =>
+      {this.alertify.success('You have liked: ' + this.user.knownAs);
+      }, error =>
+      {
+        this.alertify.error(error);
+      }
+      );
+    }
+
 }
